169e3d3ddc941d1224ec40604ff02b0b99447be0
renyajun_macmini <569906497@qq.com>
2023-06-03 09:30:38 +0800

    兼容picovioce的唤醒词

--- picovoice/wakeword.py
+++ picovoice/wakeword.py
@@ -7,30 +7,29 @@ import configparser
 config = configparser.ConfigParser()
 config.read(f"{os.getcwd()}/config.ini")
 
-PICOVOICE_API_KEY = config['Wakeword']['Picovoice_Api_Key']
-keyword_path = os.path.join(os.getcwd(),config['Wakeword']['Picovoice_Model_Path'])
 
 class PicoWakeWord:
-    def __init__(self, PICOVOICE_API_KEY, keyword_path, model_path=None):
-        self.PICOVOICE_API_KEY = PICOVOICE_API_KEY
-        self.keyword_path = keyword_path
-        self.model_path = model_path
+    def __init__(self):
+        self.PICOVOICE_API_KEY = config['Wakeword']['Picovoice_Api_Key']
+        self.keyword_path = os.path.join(os.getcwd(),config['Wakeword']['Picovoice_Model_Path'])
+        self.model_path = None
         self.porcupine = pvporcupine.create(
             access_key=self.PICOVOICE_API_KEY,
-            keyword_paths=[self.keyword_path],
-            model_path=self.model_path
+            keywords= ['porcupine', 'ok google', "picovoice", "blueberry"]
+            # keyword_paths=[self.keyword_path],           
+            # model_path=self.model_path
         )
         self.myaudio = pyaudio.PyAudio()
         self.stream = self.myaudio.open(
-            input_device_index=2,
             rate=self.porcupine.sample_rate,
-            channels=2,
+            channels=1,
             format=pyaudio.paInt16,
             input=True,
             frames_per_buffer=self.porcupine.frame_length
         )
 
     def detect_wake_word(self):
+        print('正在检测唤醒词... 按 Ctrl+C 退出') 
         audio_obj = self.stream.read(self.porcupine.frame_length, exception_on_overflow=False)
         audio_obj_unpacked = struct.unpack_from("h" * self.porcupine.frame_length, audio_obj)
         keyword_idx = self.porcupine.process(audio_obj_unpacked)
@@ -38,7 +37,7 @@ class PicoWakeWord:
 
 
 if __name__ == '__main__':
-    picowakeword = PicoWakeWord(PICOVOICE_API_KEY, keyword_path)
+    picowakeword = PicoWakeWord()
     while True:
         audio_obj = picowakeword.stream.read(picowakeword.porcupine.frame_length, exception_on_overflow=False)
         audio_obj_unpacked = struct.unpack_from("h" * picowakeword.porcupine.frame_length, audio_obj)

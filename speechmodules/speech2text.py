2d9abb91d448b90575c6b16032c24eaccbd87527
renyajun_macmini <569906497@qq.com>
2023-07-08 00:58:24 +0800

    20230708语音识别识别日语，主程序细节优化

--- speechmodules/speech2text.py
+++ speechmodules/speech2text.py
@@ -10,12 +10,20 @@ class AzureASR:
     def __init__(self):
         self.AZURE_API_KEY = config['AzureSpeech']['AZURE_API_KEY']
         self.AZURE_REGION = config['AzureSpeech']['AZURE_REGION']
-        self.speech_config = speechsdk.SpeechConfig(subscription=self.AZURE_API_KEY, region=self.AZURE_REGION)
+        self.speech_config = speechsdk.SpeechConfig(
+            subscription=self.AZURE_API_KEY,
+            region=self.AZURE_REGION
+            )
+        #self.speech_config.speech_recognition_language = "zh-CN"
 
-    def speech2text(self, audio_path: str = audio_file_path, if_microphone: bool = True):
-        self.speech_config.speech_recognition_language = "zh-CN"
+    def speech2text(self, audio_path: str = audio_file_path, if_microphone: bool = True):       
         audio_config_recognizer = speechsdk.audio.AudioConfig(use_default_microphone=True)
-        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config, audio_config=audio_config_recognizer)
+        auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig(languages=["zh-CN","en-US", "ja-JP"])
+        speech_recognizer = speechsdk.SpeechRecognizer(
+            speech_config=self.speech_config,
+            audio_config=audio_config_recognizer,
+            auto_detect_source_language_config=auto_detect_source_language_config,
+            )             
         print("system:正在聆听...")
         speech_recognition_result = speech_recognizer.recognize_once_async().get()
 

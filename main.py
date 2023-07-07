ac98e2d32e6c1586eadfe0f369b592d169fd7613
renyajun_macmini <569906497@qq.com>
2023-07-08 00:58:24 +0800

    20230708语音识别识别日语，主程序细节优化

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ config.read(os.path.join(os.getcwd(), "config.ini"),encoding="UTF-8")
 robot_info_file_path = os.path.join(os.getcwd(), "robot_info.json")
 chatgptmult = ChatGptMult()
 
+# 增加程序启动时的开机广告，并且告知用户智能音箱的唤醒词。
+print(f"system:开机广告时间，广告位长期招商！我的唤醒词是：{config['Wakeword']['wakewordtext']}")
+AzureTTS("zh-CN-XiaoxiaoNeural").text2speech_and_play(f"开机广告时间，广告位长期招商！我的唤醒词是：{config['Wakeword']['wakewordtext']}")
+
+# 这是用于判断一个字符串中，是不是包含一个列表中的任意词，如果包含就会返回列表中的这个元素。
+# 实际业务上，是判断语音转为文字的内容，是不是包含任意一个智能语音助手的激活关键词。
 def find_robot_keyword(s,lst):
     for elem in lst:
         if elem in s:
@@ -20,11 +26,13 @@ def find_robot_keyword(s,lst):
 class Yoyo:
     def __init__(self):
         with open(robot_info_file_path , 'r' ,encoding="UTF-8") as f:
+            # 导入智能助手的配置文件。
             self.robot_info = json.load(f)
             self.robot_id_list = [d['robot_id'] for d in self.robot_info]
             self.robot_keywords_list = [d['robot_keyword'] for d in self.robot_info]
 
-    def robot_model(self,robot_id="yoyo"):
+    def robot_model(self,robot_id="xiaozhushou"):
+        # 主要用于判断加载哪一个智能语音助手。
         try:
             robot_index = self.robot_id_list.index(robot_id)
         except ValueError:
@@ -38,7 +46,6 @@ class Yoyo:
         self.username = self.robot_info[robot_index ]['username']  
         self.asr = AzureASR()
         self.tts = AzureTTS(self.robot_voice_name)
-        self.tts.text2speech_and_play(f"开机广告时间，广告位长期招商！")
         self.stopword = config['Wakeword']['StopWord']
         if config['Wakeword']['WakeUpScheme'] == "Picovoice":            
             from picovoice.wakeword import PicoWakeWord
@@ -51,31 +58,35 @@ class Yoyo:
 
     def run(self):
         while True:
-            isdetected = self.wakeword.detect_wake_word()
+            isdetected = self.wakeword.detect_wake_word() # 持续监测麦克风收录的声音是不是包含唤醒词，监测到后会返回大于0的整数。如果没有监测到就持续监测。
             if isdetected >= 0:
-                print(f'{self.robot_name}:{self.robot_reply_word}')
+                # 唤醒后，打印和播放当前智能语音助手的打招呼语。
+                print(f'{self.robot_name}:{self.robot_reply_word}')  
                 self.tts.text2speech_and_play(self.robot_reply_word)
-                sleep = True            
-                while sleep:         
-                    q = self.asr.speech2text()
+                keepawake = True # 用于控制智能语音助手是不是保持持续对话模式，为假的话会进入睡眠模式，睡眠模式下需要用唤醒词重新唤醒。
+                while keepawake:         
+                    q = self.asr.speech2text() # 获取用户输入的内容，由录音转为文字。
                     if q == None or self.stopword in q:
-                        print("system:已经进入睡眠模式！")
+                        # 判断录入的内容是不是空值，或者录入的录入的内容是不是包含“停止词”。如果为空或者包含停止词，则进入睡眠模式。
+                        print(f'{self.username}:{q}')
+                        print(f"{self.robot_name}:拜拜，有事用唤醒词叫我。")
                         self.tts.text2speech_and_play(f"拜拜，有事用唤醒词叫我。")
-                        sleep = False
+                        print("system:已经进入睡眠模式！")                        
+                        keepawake = False
                     else:
-                        robot_keyword = find_robot_keyword(q,self.robot_keywords_list)
-                        if robot_keyword == None:
-                            print(f'{self.username}:{q}')
-                            res = chatgptmult.chatmult(self.username,q,self.robot_system_content)
-                            print(f'{self.robot_name}(GPT)：{res}')
-                            self.tts.text2speech_and_play(res)   
+                        robot_keyword = find_robot_keyword(q,self.robot_keywords_list) #判断用户录入的内容是不是包含任意一个智能语音助手的激活关键词。如果不包含，就请求ChatGPT的结果。如果包含，就切换到对应的智能语音助手。
+                        if robot_keyword == None:                          
+                            print(f'{self.username}:{q}') # 打印用户录入的内容
+                            res = chatgptmult.chatmult(self.username,q,self.robot_system_content) # 请求ChatGPT的接口。
+                            print(f'{self.robot_name}(GPT)：{res}')   # 打印返回的结果。
+                            self.tts.text2speech_and_play(res)   # 朗读返回的结果。
                         else:
                             switch_robot_index = self.robot_keywords_list.index(robot_keyword)
-                            switch_robot_id = self.robot_info[switch_robot_index]["robot_id"]
-                            self.robot_model(switch_robot_id)  
-                            print(f"已切换到「{robot_keyword}」,请用唤醒词重新唤醒。")
+                            switch_robot_id = self.robot_info[switch_robot_index]["robot_id"] # 确定要切换到哪一个智能语音助手。
+                            self.robot_model(switch_robot_id)   #切换智能语音助手。
+                            print(f"system:已切换到「{robot_keyword}」,请用唤醒词重新唤醒我。")
                             self.tts.text2speech_and_play(f"已切换到「{robot_keyword}」,请用唤醒词重新唤醒我。")
-                            sleep = False                                                
+                            # keepawake = False #原本切换智能语音助手后需要重新唤醒，现在应该不需要了。                                           
 
     def loop(self,):
         while True:

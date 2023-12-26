47c688342105ac2b52ecb2cfb0f700801a438588
renyajun_macmini <569906497@qq.com>
2023-12-26 22:59:21 +0800

    开始支持google的ai接口

--- googlebot/googlebotsingle.py
+++ googlebot/googlebotsingle.py
@@ -12,26 +12,15 @@ from speechmodules import text2speech
 config = configparser.ConfigParser()
 config.read(os.path.join(workdir, "config.ini"),encoding="UTF-8")
 configsection = config['google']
-GOOGLE_API_KEY = configsection["key"]
+google_api_key = configsection["key"]
 
-# Used to securely store your API key
-# from google.colab import userdata
-
-from IPython.display import display
-from IPython.display import Markdown
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
-genai.configure(api_key=GOOGLE_API_KEY)
+genai.configure(api_key=google_api_key)
 
 def modlelist():
+    # 罗列当前支持的模型名称。
     for m in genai.list_models():
         if 'generateContent' in m.supported_generation_methods:
             print(m.name)
-
-modlelist()
 
 class GoogleBotSingle:
     def __init__(self):
@@ -42,11 +31,11 @@ class GoogleBotSingle:
         model = genai.GenerativeModel('gemini-pro')
         message = prompt_messages[-1]["content"]
         response = model.generate_content(message)
-        print()
         print(response.text)
-        return to_markdown(response.text)
+        return response.text
 
 if __name__ == '__main__':
+    modlelist()
     system = "You are a helpful assistant"
     prompt = input("请输入你的问题：")
     messages=[{"role":"system","content": system},{"role": "user", "content":prompt}]
